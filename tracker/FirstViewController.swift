14e5247d3661163b4fff8809008ee02ebd45ba3a
nejosephliu <nejosephliu@gmail.com>
2016-10-21 23:32:48 -0700

    Initial Commit

---
+++ tracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tracker
+//
+//  Created by Joseph Liu on 10/21/16.
+//  Copyright © 2016 Joseph Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
