f012e5ef87b4d7cd839e0c031f06c6009b8b99ff
nejosephliu <nejosephliu@gmail.com>
2016-10-29 00:48:13 -0700

    Created Constants class

---
+++ tracker/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  tracker
+//
+//  Created by Joseph Liu on 10/29/16.
+//  Copyright © 2016 Joseph Liu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants{
+    static var lightBlueColor = UIColor(red: 96.0 / 255.0, green: 181.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
+
+}
