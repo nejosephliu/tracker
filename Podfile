9f6a64afaf4d541a69202e48087ddf7b85399842
nejosephliu <nejosephliu@gmail.com>
2017-04-13 16:21:12 -0700

    Revert "Installed SwiftSpinner pod"
    This reverts commit 680ef1e6f10c3c21cf5aeefb394ba6366bc1f671.

--- Podfile
+++ Podfile
@@ -4,12 +4,11 @@ use_frameworks!
 
 target ‘tracker’ do
     pod 'SnapKit', '~> 3.0'
-    pod 'Firebase/Core'
-    pod 'Firebase/Database'
-    pod 'Firebase/Auth'
+    pod ‘Firebase/Core'
+    pod ‘Firebase/Database’
+    pod ‘Firebase/Auth’
     pod 'Alamofire', '~> 4.0'
     pod 'DropDown', '~> 2'
     pod 'JBDatePicker', '~> 1.2'
-    pod 'SwiftSpinner', '~> 1.2'
    
 end
--- Pods/Target Support Files/Pods-tracker/Pods-tracker-frameworks.sh
+++ Pods/Target Support Files/Pods-tracker/Pods-tracker-frameworks.sh
@@ -88,12 +88,10 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/DropDown/DropDown.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JBDatePicker/JBDatePicker.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftSpinner/SwiftSpinner.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/DropDown/DropDown.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JBDatePicker/JBDatePicker.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftSpinner/SwiftSpinner.framework"
 fi
